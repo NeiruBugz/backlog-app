cfee9999a3b027dc138eb12853f1bbaa59adc031
Nail Badiullin <badiullin.nail@gmail.com>
2022-12-24 21:53:34 +0400

    infra/feature-sliced implement basic structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react'
+import './index.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -10,9 +9,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
