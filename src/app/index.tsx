ec867f7a969a2f67ed84242ae5a45f9e54008419
Nail Badiullin <badiullin.nail@gmail.com>
2022-12-24 22:02:17 +0400

    infra/feature/sliced add eslint + prettier

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './index.css'
+import { useState } from 'react';
+import './index.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -7,24 +7,20 @@ const App = () => {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount(count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
