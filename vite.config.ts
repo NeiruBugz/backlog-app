874a691ddcabbc60cc0f954848cfab3f0e537ef2
Nail Badiullin <badiullin.nail@gmail.com>
2023-01-11 14:26:54 +0400

    infra/feature-sliced update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  define: {
+    global: {},
+  },
+  server: { https: true, origin: 'https://backlog-app.localhost.com:8080' },
+  plugins: [react(), tsconfigPaths(), mkcert()]
 });
