5e85dfce9312c14a615e70207fb98192631a7cf8
Nail Badiullin <badiullin.nail@gmail.com>
2023-01-16 00:12:43 +0400

    feature/add-game mocks cleanup

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,6 +1,7 @@
 import type { Game } from '@entities';
 
 const cardProps: Game = {
+  id: 'some-id-3',
   title: 'Persona 5 Royal',
   img: 'https://assets1.ignimgs.com/2020/02/14/persona-5-royal---button-fin-1581716582492.jpg',
   platform: 'Nintendo Switch',
@@ -9,18 +10,21 @@ const cardProps: Game = {
 
 const gamesList: Game[] = [
   {
+    id: 'some-id-0',
     title: 'God of War: Ragnarök',
     img: '',
     platform: 'PlayStation 5',
     status: 'completed',
   },
   {
+    id: 'some-id-1',
     title: 'The Last Of Us Part I',
     img: '',
     platform: 'PlayStation 5',
     status: 'completed',
   },
   {
+    id: 'some-id-2',
     title: 'Forza Horizon 5',
     img: '',
     platform: 'Xbox',
